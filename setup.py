a81c55f18bed6980ea2b072ddf1c6aae048eab70
Martyn <martyn1fyles@gmail.com>
2020-04-29 10:01:07 +0100

    trying to fix setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
    #url="http://www.foopackage.com/",
    packages=['household-contact-tracing'],  #same as name
    install_requires=['networkx', 'numpy', 'scipy', 'matplotlib', 'pytest', 'pydot'], #external packages as dependencies
-   py_modules = ['household_contact_tracing']
+   #py_modules = ['household_contact_tracing']
 )
