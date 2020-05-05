b61dd80387b3c74160fa0e4a19d5083415e618f4
Martyn <martyn1fyles@gmail.com>
2020-05-05 11:37:45 +0100

    baseline simulation commit

--- Simulate_no_test_delays.py
+++ Simulate_no_test_delays.py
@@ -5,42 +5,28 @@ import numpy.random as npr
 
 # npr.seed(1)
 
-repeats = 200
+repeats = 100
 days_to_simulate = 25
 starting_infections = 5000
 
-# control parameters
-infection_detect_prob = [0.2, 0.4, 0.6, 0.8]
-haz_rate_range = [0.816914, 0.819325, 0.809742, 0.806271]
-
-par_range = [
-    (0.2, 0.816914),
-    (0.4, 0.819325),
-    (0.6, 0.809742),
-    (0.6, 0.806271)
-]
-
 def run_simulation(repeat):
 
-    # Choose a random case detection probability
-    random_index = npr.choice(range(4))
+    haz_rate_scale = 0.806271
 
-    infection_reporting_prob, haz_rate_scale = par_range[random_index]
+    infection_reporting_prob = 0.8
 
     contact_tracing_success_prob = npr.uniform(0.7, 0.95)
 
     contact_trace_delay_par = npr.uniform(1.5, 2.5)
 
-    infection_reporting_prob = npr.uniform(0.2, 0.7)
-
-    reduce_contacts_by = npr.uniform(0.4, 0.9)
+    reduce_contacts_by = npr.uniform(0.0, 0.8)
 
     do_2_step = npr.choice([True, False])
 
     prob_has_trace_app = npr.uniform(0, 0.5)
 
     simulation = model.household_sim_contact_tracing(haz_rate_scale=haz_rate_scale,
-                                                     contact_tracing_success_prob=infection_reporting_prob,
+                                                     contact_tracing_success_prob=contact_tracing_success_prob,
                                                      contact_trace_delay_par=contact_trace_delay_par,
                                                      overdispersion=0.36,
                                                      infection_reporting_prob=infection_reporting_prob,
@@ -80,7 +66,7 @@ for i in range(len(col_names)):
     col_names_dict.update({i: col_names[i]})
 
 if __name__ == '__main__':
-    with Pool(14) as p:
+    with Pool() as p:
         results = p.map(run_simulation, range(repeats))
         results = pd.DataFrame(results)
         results = results.rename(columns=col_names_dict)
