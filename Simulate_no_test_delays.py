aa24c5bcfec83f0722026c67081dd126f2015aa9
Martyn <martyn1fyles@gmail.com>
2020-05-12 14:51:04 +0100

    Added the different phases and calibration dict

--- Simulate_no_test_delays.py
+++ Simulate_no_test_delays.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool
 import household_contact_tracing as model
 import pandas as pd
+import pickle
 import numpy.random as npr
 
 # npr.seed(1)
@@ -9,17 +10,32 @@ repeats = 100
 days_to_simulate = 25
 starting_infections = 5000
 
+# Importing the calibration dictionary
+with open('Data/Calibration/hazard_rate_detection_prob_pairs.pickle', 'rb') as handle:
+    pairs_dict = pickle.load(handle)
+
 def run_simulation(repeat):
 
-    haz_rate_scale = 0.806271
+    infection_reporting_prob = npr.choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
-    infection_reporting_prob = 0.8
+    haz_rate_scale = pairs_dict[infection_reporting_prob]
 
     contact_tracing_success_prob = npr.uniform(0.7, 0.95)
 
     contact_trace_delay_par = npr.uniform(1.5, 2.5)
 
     reduce_contacts_by = npr.uniform(0.0, 0.8)
+
+    # phase 1
+    # reduce_contacts_by=(0.890642616, 0.899481865, 0.924573127, 0.941991031, 0.943881139, 0.947219841)
+    # scenario 1 phase 2
+    # reduce_contacts_by=(0.861092459, 0.872320219, 0.892143, 0.911016087, 0.909812177, 0.912316683)
+    # scenario 2 phase 2
+    # reduce_contacts_by=(0.861092459, 0.872320219, 0.873348547, 0.887149766, 0.880322821, 0.880118778)
+    # phase 3
+    # reduce_contacts_by=(0.80929767, 0.824711951, 0.825994539, 0.844906993, 0.835495821, 0.83519708)
+    # phase 4
+    reduce_contacts_by=(0.703027714, 0.727031692, 0.711877922, 0.736701059, 0.716914094, 0.713977162)
 
     do_2_step = npr.choice([True, False])
 
@@ -70,4 +86,4 @@ if __name__ == '__main__':
         results = p.map(run_simulation, range(repeats))
         results = pd.DataFrame(results)
         results = results.rename(columns=col_names_dict)
-        results.to_excel("Data/simulation_results_no_test_delays.xlsx")
+        results.to_excel("Data/Exit Strategy/phase4.xlsx")
